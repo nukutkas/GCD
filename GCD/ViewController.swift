6f087afd7791ece679d6e27465c96ec38774df0d
Nikita Kochetov <nothsmple@mail.ru>
2020-09-23 09:15:42 +0300

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Татьяна Кочетова on 23.09.2020.
+//  Copyright © 2020 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
